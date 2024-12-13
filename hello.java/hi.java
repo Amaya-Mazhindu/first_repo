3c8d189c605a90fdbb94f30f1c6d685990702b10
Amaya-Mazhindu <mpatronnela65@gmail.com>
2024-12-13 14:57:47 +0200

    first attempt

---
+++ hello.java/hi.java
@@ -0,0 +1,17 @@
+public class hi {
+    public static void main(String[] args) { 
+        System.out.println("Sugar pop \t!");
+        // opens a new tab 
+
+        System.out.println("Sugar pop \n sugra \n is \n crazy");
+        // \n makes the next word to go to the next line and leaves a space on the next word
+        System.out.println("Sugar pop \"i love sugar\"");
+        // this line makes sure that you print out the words in qoutation 
+    }    
+    
+}
+
+
+
+
+
